b38794d54476d74d9baa3a9b4e0be47d102520e4
Blake Walsh <blakeofthepath@gmail.com>
2018-09-27 00:10:17 +0300

    Updates to fixer script

--- .script/common.py
+++ .script/common.py
@@ -1,5 +1,16 @@
 import pathlib
+import regex
 
 from translate.storage.po import pofile
 
 PO_DIR = pathlib.Path(__file__).parent.parent
+
+def numericsortkey(string, _split=regex.compile(r'(\d+)').split):
+    # if regex.fullmatch('\d+', s) then int(s) is valid, and vice-verca.
+    return [int(s) if i % 2 else s for i, s in enumerate(_split(str(string)))]
+
+
+def humansortkey(string, _split=regex.compile(r'(\d+(?:[.-]\d+)*)').split):
+    # With split, every second element will be the one in the capturing group.
+    return [numericsortkey(s) if i % 2 else s
+            for i, s in enumerate(_split(str(string)))]
--- .script/sanity_checker.py
+++ .script/sanity_checker.py
@@ -1,6 +1,6 @@
 import regex
 
-from common import PO_DIR, pofile
+from common import PO_DIR, pofile, humansortkey
 
 
 class Num:
@@ -36,16 +36,53 @@ def is_1_greater(a, b, uid, original_con):
         print(f"{uid}:{original_con}   {'.'.join(str(n) for n in a)} -> {'.'.join(str(n) for n in b)}")
 
 
+def renumber_zeros(po):
+    i = 1
+    for unit in po.units:
+        msgctxt = unit.msgctxt
+        if not msgctxt:
+            continue
+        uid, num = msgctxt[0][1:-1].split(':')
+        
+        if num.startswith('0'):
+            unit.msgctxt[0] = f'"{uid}:0.{i}"'
+            i += 1
+        else:
+            return i > 1
+
+def compare_order(a, b):
+    nums_a = [l[1] for l in a]
+    nums_b = [l[1] for l in b]
+    if nums_a == nums_b:
+        return False
+    for i, j in zip(nums_a, nums_b):
+        if i != j:
+            print(f'{a[0][0]}: Sort Mismatch {i} != {j}')
+            return
+    
+
+def check_ordering(contexts, file):
+    
+    compare_order(contexts, sorted(contexts, key=humansortkey))
+    compare_order(contexts, sorted(reversed(contexts), key=humansortkey))
+    
+
 for file in sorted(PO_DIR.glob('pli-tv/**/*.po')):
     with file.open('r') as f:
-        po = pofile(f)
+        po = pofile(f)  
+    
+    changed = False
+    if 'np' in file.name:
+        changed = renumber_zeros(po)
     
     contexts = []
-    for unit in po.units:
-        
+    for unit in po.units:    
         msgctxt = unit.msgctxt
         if msgctxt:
             contexts.append(msgctxt[0][1:-1].split(':'))
+            
+    check_ordering(contexts, file)
+    
     nums = []
     last_nums = None
     for uid, con in contexts:
